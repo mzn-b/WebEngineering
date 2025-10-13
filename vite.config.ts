e573a98689333c637d89c0b38a4fbd0977674f28
mzn-b <medomedso300@gmail.com>
2025-10-13 15:36:33 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
+    : "/",
   server: { port: 5173 },
   build: {
     outDir: "dist",
